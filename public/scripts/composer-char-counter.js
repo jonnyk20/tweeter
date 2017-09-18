ab0d7691c3c2936308bcf08a93bfdc62e48d3bdc
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-18 21:59:27 +0000

    fixed linting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
-$(function(){
+$(function foo() {
   const charMax = 140;
-  $('.new-tweet').on('input','textarea', function() {
+  $('.new-tweet').on('input', 'textarea', () => {
     const $this = $(this);
-    let length = $this.val().length;
-    let counterSpan = $this.closest('.new-tweet').find('.counter');
-    counterSpan.text( 140 - length );
-    if ( length > charMax ) {
+    const { length } = $this.val();
+    const counterSpan = $this.closest('.new-tweet').find('.counter');
+    counterSpan.text(140 - length);
+    if (length > charMax) {
       counterSpan.addClass('error');
     } else {
       counterSpan.removeClass('error');
